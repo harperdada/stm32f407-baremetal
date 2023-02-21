1499ff28493cbf7eaf77e3234ff5d8954bf0af55
Harper Dada <harperkukuar@gmail.com>
2023-02-21 14:30:41 -0800

    Too bare; please wear some clothes

---
+++ too_bare/main.c
@@ -0,0 +1,72 @@
+// Copyright (c) 2022 Cesanta Software Limited
+// All rights reserved
+
+#include <inttypes.h>
+#include <stdbool.h>
+
+#define BIT(x) (1UL << (x))
+#define PIN(bank, num) ((((bank) - 'A') << 8) | (num))
+#define PINNO(pin) (pin & 255)
+#define PINBANK(pin) (pin >> 8)
+
+struct rcc {
+  volatile uint32_t CR, PLLCFGR, CFGR, CIR, AHB1RSTR, AHB2RSTR, AHB3RSTR,
+      RESERVED0, APB1RSTR, APB2RSTR, RESERVED1[2], AHB1ENR, AHB2ENR, AHB3ENR,
+      RESERVED2, APB1ENR, APB2ENR, RESERVED3[2], AHB1LPENR, AHB2LPENR,
+      AHB3LPENR, RESERVED4, APB1LPENR, APB2LPENR, RESERVED5[2], BDCR, CSR,
+      RESERVED6[2], SSCGR, PLLI2SCFGR;
+};
+#define RCC ((struct rcc *) 0x40023800)
+
+struct gpio {
+  volatile uint32_t MODER, OTYPER, OSPEEDR, PUPDR, IDR, ODR, BSRR, LCKR, AFR[2];
+};
+#define GPIO(bank) ((struct gpio *) (0x40020000 + 0x400 * (bank)))
+
+// Enum values are per datasheet: 0, 1, 2, 3
+enum { GPIO_MODE_INPUT, GPIO_MODE_OUTPUT, GPIO_MODE_AF, GPIO_MODE_ANALOG };
+
+static inline void gpio_set_mode(uint16_t pin, uint8_t mode) {
+  struct gpio *gpio = GPIO(PINBANK(pin));  // GPIO bank
+  int n = PINNO(pin);                      // Pin number
+  gpio->MODER &= ~(3U << (n * 2));         // Clear existing setting
+  gpio->MODER |= (mode & 3U) << (n * 2);   // Set new mode
+}
+
+static inline void gpio_write(uint16_t pin, bool val) {
+  struct gpio *gpio = GPIO(PINBANK(pin));
+  gpio->BSRR |= (1U << PINNO(pin)) << (val ? 0 : 16);
+}
+
+static inline void spin(volatile uint32_t count) {
+  while (count--) asm("nop");
+}
+
+int main(void) {
+  uint16_t led = PIN('D', 15);            // Blue LED
+  RCC->AHB1ENR |= BIT(PINBANK(led));     // Enable GPIO clock for LED
+  gpio_set_mode(led, GPIO_MODE_OUTPUT);  // Set blue LED to output mode
+  for (;;) {
+    gpio_write(led, true);
+    spin(999999);
+    gpio_write(led, false);
+    spin(999999);
+  }
+  return 0;
+}
+
+// Startup code
+__attribute__((naked, noreturn)) void _reset(void) {
+  // memset .bss to zero, and copy .data section to RAM region
+  extern long _sbss, _ebss, _sdata, _edata, _sidata;
+  for (long *src = &_sbss; src < &_ebss; src++) *src = 0;
+  for (long *src = &_sdata, *dst = &_sidata; src < &_edata;) *src++ = *dst++;
+
+  main();             // Call main()
+  for (;;) (void) 0;  // Infinite loop in the case if main() returns
+}
+
+extern void _estack(void);  // Defined in link.ld
+
+// 16 standard and 91 STM32-specific handlers
+__attribute__((section(".vectors"))) void (*tab[16 + 91])(void) = {_estack, _reset};
